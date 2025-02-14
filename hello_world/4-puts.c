0d730cf0953a4bb829162ec87577dea23132bdf5
Ancelin-31 <10496@holbertonstudents.com>
2025-02-14 10:29:42 +0000

    syntax

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * puts - prints "Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual puzzle
  *
  * Return: zero
  */
