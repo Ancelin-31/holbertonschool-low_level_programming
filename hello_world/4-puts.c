8a2f7b449f7cd95b8742f819617d1fd79f58400a
Ancelin-31 <10496@holbertonstudents.com>
2025-02-14 10:13:14 +0000

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * puts - prints "Programming is like building a multilingual puzzle
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
