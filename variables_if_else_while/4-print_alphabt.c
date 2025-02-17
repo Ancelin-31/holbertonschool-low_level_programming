c254a94b4181e42e1a09c91ff5c86c628a0e7708
Ancelin-31 <10496@holbertonstudents.com>
2025-02-17 15:06:28 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - displays alphabet in lowercase except c and q
+ *
+ * Return: 0 if correct
+ */
+int main(void)
+{
+	int alphabt = 'a';
+
+	for (; alphabt < 'z'; alphabt++)
+	{
+		if (alphabt == 'e' || alphabt == 'q')
+		{
+		}
+		else
+		{
+		putchar(alphabt);
+		}
+	}
+putchar('\n');
+return (0);
+}
