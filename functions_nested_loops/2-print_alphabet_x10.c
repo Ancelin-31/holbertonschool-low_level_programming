2084ed44c9fe0e1bb0067c7a6c068fe7e9798afe
Ancelin-31 <10496@holbertonstudents.com>
2025-02-19 15:24:07 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,18 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int alphabet;
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		alphabet = 97;
+
+		while (alphabet <= 122)
+		{
+			_putchar(alphabet);
+
+			alphabet++;
+		}
+	_putchar('\n');
 	}
 }
