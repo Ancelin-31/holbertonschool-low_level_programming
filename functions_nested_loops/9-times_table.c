ee562e44a39f277b422a16e6356a07729eed4b92
Ancelin-31 <10496@holbertonstudents.com>
2025-02-21 15:28:29 +0000

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *times_table - is to list all minutes in a day
+ *@:
+ *Return: no need
+ */
+void times_table(void)
+{
+	int table;
+
+	for (table = 0; table < 10;)
+	{
+		int multiple;
+
+		for (multiple = 0; multiple < 10; multiple++)
+		{
+			int result = table * multiple;
+
+			if (multiple == 0)
+			{
+			_putchar(result + '0');
+			}
+			else
+			{
+			if (result < 10)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(result + '0');
+			}
+			else
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(result / 10 + '0');
+			_putchar(result % 10 + '0');
+			}
+			}
+		}
+		_putchar('\n');
+		table++;
+	}
+}
