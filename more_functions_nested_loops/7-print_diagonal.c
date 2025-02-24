9abca60d0294170d97a371ea154374e921ddb023
Ancelin-31 <10496@holbertsonstudents.com>
2025-02-24 16:43:14 +0100

    setup 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal of base n
+ * @n: int
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(i * ' ');
+		_putchar('\');
+		_putchar('\n');
+	}
+}
