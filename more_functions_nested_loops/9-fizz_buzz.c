c7ec2b283b15d53b1d05ab1c3c0132988c504533
Ancelin-31 <10496@holbertsonstudents.com>
2025-02-25 11:19:36 +0100

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - prints from 1 to 100 with fizz and buzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
