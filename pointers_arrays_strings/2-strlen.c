4cc36c91f965e640a4ea32b1935920012b703379
Ancelin-31 <10496@holbertsonstudents.com>
2025-02-26 15:27:27 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *@s: char
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int length;
+	
+	for (length = 0; s[length];)
+	{
+		length++;
+	}
+	return (length);
+}
