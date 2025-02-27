4ec7c8516cff3d22ca9e2e949ad534307120c481
Ancelin-31 <10496@holbertsonstudents.com>
2025-02-27 15:16:04 +0100

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string of characters
+ */
+
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = _strlen(s) - 1;
+	char temp;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
