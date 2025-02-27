405dd0aebc4105eafad0f8e86d5421d6cb145df0
Ancelin-31 <10496@holbertsonstudents.com>
2025-02-27 16:24:25 +0100

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: string of characters
+ *
+ */
+void puts_half(char *str)
+{
+	int length;
+	int half;
+
+	for (length = 0; str[length]; length++)
+	{
+	}
+	
+	if (length % 2 == 0)
+	{
+		for (half = (length / 2); half < length ; half++)
+		{
+			_putchar(str[half]);
+		}
+	}
+	else
+	{
+		for (half = ((length + 1) / 2); half < length ; half++)
+		{
+			_putchar(str[half]);
+		}
+	}
+	_putchar('\n');
+}
