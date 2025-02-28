94ee12bd2f26019b197d8cd32a9b5c308050ef97
Ancelin-31 <10496@holbertsonstudents.com>
2025-02-28 16:35:54 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,38 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int size_s1;
-	int size_s2;
-	int comp;
-	int result;
-	int limit;
+	int i;
 
-	for (size_s1 = 0; s1[size_s1]; size_s1++)
+	for (i = 0; s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-	}
-	for (size_s2 = 0; s2[size_s2]; size_s2++)
-	{
-	}
-	if (size_s1 <= size_s2)
-	{
-		limit = size_s1;
-	}
-	else
-	{
-		limit = size_s2;
 	}
 
-	while (comp <= limit)
-	{
-		if (s1[comp] == s2[comp])
-		{
-			comp++;
-		}
-		else
-		{
-			result = s1[comp] - s2[comp];
-		}
-		comp++;
-	}
-	return (result);
+	return (*(s1 + i) - *(s2 + i));
 }
