784af7a8f2873c952597803e444263f92237334f
Ancelin-31 <10496@holbertsonstudents.com>
2025-03-03 15:31:37 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 	char *ptr;
-
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -26,5 +26,5 @@ char *_strchr(char *s, char c)
 			return ('\0');
 		}
 	}
-	return ('\0');
+	return (&s[i]);
 }
