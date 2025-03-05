18bf906f8e08aa012528aae56c12d28a9ec79262
Ancelin-31 <10496@holbertsonstudents.com>
2025-03-05 09:54:25 +0100

    7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_strlen.c"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: pointer to pointer (array 8x8)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int height;
+	int width;
+
+	for (height = 0; height < 8; height++)
+	{
+		for (width = 0; width < 8; width++)
+		{
+			_putchar(a[height][width]);
+		}
+		_putchar('\n');
+	}
+}
