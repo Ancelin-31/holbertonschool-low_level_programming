7ccee6c0cb5b493bcd7f11d1858e7352862c02d3
root <root@Duat.>
2025-03-09 22:27:26 +0100

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * followed by a new line
+ * @argc: number of arguments
+ * @aergv: array of pointers
+ *
+ * Return: 0 if true
+ */
+
+int main(int argc __attribute__((__unused__)), char *argv[]))
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,12 @@
+/**
+ * header containing prototypes
+ * linking with argc and argv
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void _putchar(char);
+int _strlen(char *s);
+
+#endif
