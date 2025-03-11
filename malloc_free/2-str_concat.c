5d7c605164acb9e073973d36305907eabc9805f8
root <root@Duat.>
2025-03-11 11:06:16 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,9 +12,39 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int size1;
-	unsigned int size2;
+	int size1;
+	int size2;
 	int i;
 	char *stotal;
 
+	if (s1 == NULL)
+		s1 = " ";
 
+	if (s2 == NULL)
+		s2 = " ";
+
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+	{
+	}
+
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+	{
+	}
+
+	stotal = malloc(sizeof(char) * (size1 + size2) + 1);
+
+	if (stotal == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1; i++)
+		stotal[i] = s1[i];
+
+	for (i = 0; i <= size2; i++)
+	{
+		if (i != size2)
+			stotal[size1 + i] = s2[i];
+		else
+			stotal[size1 + i] = '\0';
+	}
+	return (stotal);
+}
