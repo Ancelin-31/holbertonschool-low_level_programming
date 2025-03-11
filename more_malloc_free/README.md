fa9119a2d4dd9dba5daf3790ad7f423538dbc4b1
root <root@Duat.>
2025-03-11 20:45:53 +0100

    0-malloc_checked.c

---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Let's dive deeper into the functions of malloc and free
---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+/**
+ * header containing more prototypes concerning
+ * malloc and free functions
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void _putchar(char);
+int _strlen(char *s);
+void *malloc_checked(unsigned int b);
+
+#endif
