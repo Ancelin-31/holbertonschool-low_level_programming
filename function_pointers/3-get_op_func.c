b3621e757da852228f1c0e60ed049ca695dcb90c
root <root@Duat.>
2025-03-18 15:44:53 +0100

    3-calc.h

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,25 +1,32 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - selects the correct function to perform the operation asked by the user,
- * accordingly to the operator used
+ * get_op_func - selects the correct function to perform the operation
+ * asked by the user accordingly to the operator used
  * @s: sign operator
- * @int: numbers to combine
  *
  * Return: pointer to correct function (or NULL if error)
  */
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    	int i;
+		op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+		};
+		int i;
 
+		for (i = 0; i < 5; i++)
+		{
+			if (strcmp(ops[i].op, s) == 0)
+				return (ops[i].f);
+		}
+		return (NULL);
+}
