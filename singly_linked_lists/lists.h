f87dd2b39f074eb7d1fe4d6b0024d1ed8bf09418
root <root@Duat.>
2025-04-01 10:07:53 +0200

    define struct

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,11 +1,20 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef LISTS_H_
+#define LISTS_H_
 
+#include <stddef.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
+
+int _putchar(char c);
 size_t print_list(const list_t *h);
 
 
