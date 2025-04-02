2ad1ca1f6872a4204b7310f5b6931b4af38c06d9
root <root@Duat.>
2025-04-02 20:09:19 +0200

    task 1

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -34,6 +34,8 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
