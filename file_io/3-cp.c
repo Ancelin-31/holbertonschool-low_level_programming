f2c4a45c4546f66869bf6a2b50b69187d57624e9
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-04-04 17:39:50 +0200

    deleted file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,36 +35,36 @@ int main(int argc, char **argv)
 
 void cp(const char *src, const char *dest)
 {
-	int file_src, file_dest, file;
+	int file_src, file_dest;
 	char buffer[1024];
 	ssize_t bytes_read = 0, bytes_written = 0;
 
 	file_src = open(src, O_RDONLY, 0444);
 	file_dest = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	while (bytes_read > 0)
+	while ((bytes_read = read(file_src, buffer, sizeof(buffer))) > 0)
 	{
+		bytes_written = write(file_dest, buffer, bytes_read);
 		if (file_dest == -1 || bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			close(file_src);
+			close(file_dest);
 			exit(99);
 		}
-
-		bytes_read = read(file_src, buffer, 1024);
 	}
 
 	if (file_src == -1 || bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		close(file_src);
+		close(file_dest);
 		exit(98);
 	}
 
-	file = close(file_src);
-	file = close(file_dest);
-
-	if (file == -1)
+	if (close(file_dest) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_dest);
 		exit(100);
 	}
 }
